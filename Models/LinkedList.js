f1d21001a3ea23d97c6ba796d8ee01c585bb830f
David Prieto <david.s.prieto1@gmail.com>
2023-05-01 12:02:39 -0500

    initial commit

---
+++ Models/LinkedList.js
@@ -0,0 +1,20 @@
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push (value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let myLinkedList = new LinkedList(7);
+myLinkedList.push(4);
+console.log(myLinkedList);
